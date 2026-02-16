c77f4806dded397d1b4945b21810348f916ca79e
Abdulkarem Ghassan <abdulkaremghassan@gmail.com>
2026-02-16 21:33:33 +0300

    Configure for root domain deployment

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   reactStrictMode: true,
-  basePath: "/portfolio", // <=== Required for project pages (e.g. user.github.io/repo-name)
   images: {
     unoptimized: true,
   },
